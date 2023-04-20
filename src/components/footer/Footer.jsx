b6f922d69893134f75ae34ddc4cadf317cc5e0d3
temirlan <tb@plazz.ag>
2023-04-20 18:34:11 +0200

    footer added

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import SocialList from "./SocialList";
+import "./footer.css"
+
+
+const Footer = () => {
+
+  const [social, setSocial] = useState([
+    {"img": "./img/svg/Mask whatsapp.svg"}, 
+    {"img": "./img/svg/Mask instagram.svg"}, 
+    {"img": "./img/svg/Mask facebook.svg"}, 
+    {"img": "./img/svg/Mask telegram.svg"}, 
+   
+  ])
+
+return ( 
+    <footer className="footer">
+      <div className="wrapper">
+        <div className="logo">Logo</div>
+
+        <SocialList  social ={social}/>
+
+        <div className="footer__cantacts">
+          <div className="contact-tel">+996 709 91 95 82</div>
+          <div className="contact-address">ул.Ak-cholmok 115/1</div>
+          <div className="contact-email">cleaningBishkek@gmail.com</div>
+        </div>
+      </div>
+    </footer>
+)
+}
+
+export default Footer;
